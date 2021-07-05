ca06dbdb4e51d231c093e5c1722981623f80d396
sebastian.kb@wp.pl <sebastian.kb@wp.pl>
2021-07-05 20:19:47 +0200

    API content downloading

--- src/components/PlanetsList/index.vue
+++ src/components/PlanetsList/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="PlanetsList">
-    {{info}}
+    <div v-for="items in info" class="name" v-bind:key="items.id" >
+      <span>name: {{ items.name }} </span><br>
+      <span>rotation period: {{ items.rotation_period }} </span><br>
+      <span>climate: {{ items.climate }} </span><br>
+      <span>gravity: {{ items.gravity }} </span><br>
+      <span>created: {{ items.created }} </span><br>
+      <span>url: {{ items.url }} </span><br><br>
+    </div>
   </div>
 </template>
 <style src="./style.sass" lang="sass" scoped></style>
