7c4dc71ff6ff08966f3dbe95766ff075cb163534
sebastian.kb@wp.pl <sebastian.kb@wp.pl>
2021-07-06 19:05:26 +0200

    added styling

--- src/components/PlanetsList/index.vue
+++ src/components/PlanetsList/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="PlanetsList">
+  <div class="PlanetsList"><h1 class="title">Planets</h1>
     <div v-for="items in info" class="name" v-bind:key="items.id" >
-      <span>name: {{ items.name }} </span><br>
-      <span>rotation period: {{ items.rotation_period }} </span><br>
-      <span>climate: {{ items.climate }} </span><br>
-      <span>gravity: {{ items.gravity }} </span><br>
-      <span>created: {{ items.created }} </span><br>
-      <span>url: {{ items.url }} </span><br><br>
+      <div class="name-element-title"> {{ items.name }} </div><br>
+      <div class="name-element">rotation period: {{ items.rotation_period }} </div><br>
+      <div class="name-element">climate: {{ items.climate }} </div><br>
+      <div class="name-element">gravity: {{ items.gravity }} </div><br>
+      <div class="name-element">created: {{ items.created }} </div><br>
+      <div class="name-element">url: {{ items.url }} </div><br>
     </div>
   </div>
 </template>
